0a25eec583903b48414a4e0be05353858fd245bc
Vincent Duong <vincent.duong.dev@gmail.com>
2022-05-15 10:53:54 +0200

    fix

--- i3.sh
+++ i3.sh
@@ -1,4 +1,4 @@
 pacman --noconfirm -Sy i3-wm i3blocs i3lock i3status dmenu redshift
 echo "exec i3" >> ~/.xinitrc
-mkdir -p ~/.config/i3 && cp config/i3 ~/.config/i3/config && mkdir -p ~/.config/i3status 
-&& cp config/i3status ~/.config/i3status/config
+mkdir -p ~/.config/i3 && cp config/i3 ~/.config/i3/config 
+mkdir -p ~/.config/i3status && cp config/i3status ~/.config/i3status/config
