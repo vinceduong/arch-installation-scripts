95b38790dd0d2ae6bf983f66d800254f886fbf4a
Vincent Duong <vincent.duong.dev@gmail.com>
2022-05-15 11:03:13 +0200

    fix

--- snap.sh
+++ snap.sh
@@ -1,4 +1,6 @@
-pacman --noconfirm -Sy snapd
-sudo systemctl enable --now snapd.socket
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+sudo systemctl enable --now snapd.socketpacman
 snap install postman slack dbeaver-ce
 snap install kontena-lens --classic
