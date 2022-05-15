a91e09844456a53d3bcc8392de61dc5d816f46b3
Vincent Duong <vincent.duong.dev@gmail.com>
2022-05-15 10:57:58 +0200

    fix

--- alacritty.sh
+++ alacritty.sh
@@ -1,3 +1,4 @@
 pacman --noconfirm -Sy alacritty 
-cp config/alacritty ~/.alacritty.yml
+cp config/alacritty.yml ~/.alacritty.yml
+mkdir -r ~/.config/alacritty && cp config/aura-theme.yml ~/.config/alacritty
 
